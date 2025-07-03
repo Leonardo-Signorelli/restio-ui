a54af39cb4ee3d40de068e02cb541ccd24d95a2e
Leonardo-Signorelli <leonardo.signorelliv@gmail.com>
2025-07-03 14:34:34 +0200

    feat: add export

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,6 @@ export type { OTPProps } from './components/otp/otp-types';
 
 export { Button } from './components/button/button';
 export type { ButtonProps } from './components/button/button-types'
+
+export {ContributionHeatmap } from './components/contribution-heatmap/contribution-heatmap'
+export type { ContributionHeatmapProps } from './components/contribution-heatmap/contribution-heatmap-types'
